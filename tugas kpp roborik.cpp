311a28ab6280d3b8fffefa522744bf482bb7dcf5
stevanza <64017689+stevanza@users.noreply.github.com>
2022-10-12 20:27:04 +0700

    Add files via upload

---
+++ tugas kpp roborik.cpp
@@ -0,0 +1,32 @@
+// NAMA		: stevanza gian maheswara
+// NRP		: 5025221248
+// Jurusan	: teknik informatika
+
+#include <iostream>
+#include <cmath>
+
+#define GRAVITASI 10 //10 m/s^2
+#define START_PENGUKURAN 1 //pengukuran dimulai dari 1 meter
+#define SUDUT 45 //sudut elevasi tembakan
+
+tipe_data mencari_V0(tipe_data variabel1, tipe_data variabel2)
+{
+	/* Tulis fungsi mencari v0 kalian disini */
+  	return 
+}
+
+tipe_data speed_dgn_loss(tipe_data variabel)
+{
+	/* tulis fungsi hitung_loss kalian disini */
+    return
+}
+
+int main() {
+    /* tulis kode utama kalian disini */
+  
+  	/* input adalah kecepatan tangensial maksimum roller */
+  	/* std::cin >> input */
+  
+  	/* std::cout << jarak << " " << kecepatan tangensial << std::endl */
+    return 0;
+}
